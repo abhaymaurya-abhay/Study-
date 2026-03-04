aee9ec1aba9372ad53a5eeba3a650f8cd25d3c9c
Abhay Maurya <computerabhaymaurya@gmail.com>
2026-03-05 05:15:28 +0530

    Refactor server.js for cleaner code structure

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const { MongoClient } = require("mongodb");
 
 const app = express();
 
-/* ---------- Middleware ---------- */
 app.use(express.json());
 app.use(express.static(__dirname));
 
-/* ---------- MongoDB ---------- */
 const uri = "mongodb+srv://abhaymauryafb_db_user:Z7T209jAG8yBAcoq@abhay.m7i5gvj.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
@@ -16,45 +14,41 @@ const client = new MongoClient(uri);
 let studentsCollection;
 
 async function start(){
-  await client.connect();
-  const db = client.db("studentDB");
-  studentsCollection = db.collection("students");
-  console.log("MongoDB Connected");
+
+await client.connect();
+
+const db = client.db("studentDB");
+
+studentsCollection = db.collection("students");
+
+console.log("MongoDB Connected");
+
 }
+
 start();
 
-/* ---------- Routes ---------- */
-
-/* Home page */
-app.get("/", (req,res)=>{
-  res.sendFile(path.join(__dirname,"index.html"));
+app.get("/",(req,res)=>{
+res.sendFile(path.join(__dirname,"index.html"));
 });
 
-/* Add Student */
-app.post("/addStudent", async (req,res)=>{
-  try{
-    await studentsCollection.insertOne(req.body);
-    res.json({message:"saved"});
-  }catch(e){
-    console.log(e);
-    res.status(500).json({error:"save failed"});
-  }
+app.post("/addStudent",async(req,res)=>{
+
+await studentsCollection.insertOne(req.body);
+
+res.json({status:"saved"});
+
 });
 
-/* Get Students */
-app.get("/students", async (req,res)=>{
-  try{
-    const data = await studentsCollection.find().toArray();
-    res.json(data);
-  }catch(e){
-    console.log(e);
-    res.status(500).json([]);
-  }
+app.get("/students",async(req,res)=>{
+
+const data = await studentsCollection.find().toArray();
+
+res.json(data);
+
 });
 
-/* ---------- Server ---------- */
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-  console.log("Server running on port " + PORT);
+app.listen(PORT,()=>{
+console.log("Server running");
 });
