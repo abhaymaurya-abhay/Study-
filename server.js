3dddfad3930160fb6c3151e241a6c9fe39f9e1a1
Abhay Maurya <computerabhaymaurya@gmail.com>
2026-03-05 04:52:31 +0530

    Refactor server.js for better organization
    Refactor server.js to improve structure and connection handling.

--- server.js
+++ server.js
@@ -1,47 +1,12 @@
-const express = require("express")
-const { MongoClient } = require("mongodb")
+const express = require("express");
+const app = express();
 
-const app = express()
-app.use(express.json())
-app.use(express.static("public"))
+app.get("/", (req,res)=>{
+res.sendFile(__dirname + "/index.html");
+});
 
-const uri = "mongodb+srv://abhaymauryafb_db_user:PASSWORD@abhay.m7i5gvj.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 3000;
 
-const client = new MongoClient(uri)
-
-let students
-
-async function start(){
-
-await client.connect()
-
-const db = client.db("coachingDB")
-students = db.collection("students")
-
-console.log("MongoDB connected")
-
-}
-
-start()
-
-app.post("/addStudent", async(req,res)=>{
-
-await students.insertOne(req.body)
-
-res.send("Student Added")
-
-})
-
-app.get("/students", async(req,res)=>{
-
-let data = await students.find().toArray()
-
-res.json(data)
-
-})
-
-app.listen(3000, ()=>{
-
-console.log("Server running")
-
-})
+app.listen(PORT,()=>{
+console.log("Server running");
+});
